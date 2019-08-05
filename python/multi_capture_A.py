59a98834bd6dbe0055ef24562e347794912b6926
junaidchatoor1 <52464071+junaidchatoor1@users.noreply.github.com>
2019-08-05 15:29:49 -0700

    Update multi_capture_A.py
    Accidentally put the wrong code, this is the one from Friday 08/02/19

--- python/multi_capture_A.py
+++ python/multi_capture_A.py
@@ -1,195 +1,121 @@
-#Working code for remote monitoring purposes of products by inHouse Produce. Not to be shared with anyone besides co-workers
+#Working code for remote monitoring purposes of products by inHouse Produce. ONLY to be used by inHouse Produce.
 #Test code taken from https://www.arducam.com/multi-camera-adapter-module-raspberry-pi/ and modified to requirements
 
 #The following code uses an ArduCam header to multiplex four cameras at 30 minute intervals and sends the received picture right to the Amazon S3 web server before deleting it on the pi itself
 
-#import relevant modules
+#importing relevant modules
 import RPi.GPIO as gp
 import os
 import time
 import datetime
 from picamera import PiCamera
 
+#using camera as the variable to control PiCamera()
 camera = PiCamera()
 
+#set warnings OFF
 gp.setwarnings(False)
 gp.setmode(gp.BOARD)
 
+#Pin 7 needed for all cameras
 gp.setup(7, gp.OUT)
-gp.setup(11, gp.OUT)
-gp.setup(12, gp.OUT)
 
-gp.setup(15, gp.OUT)
-gp.setup(16, gp.OUT)
-gp.setup(21, gp.OUT)
-gp.setup(22, gp.OUT)
+#Listed pins according to arducam adapter number
+arducam_pins = {
+     1: [11, 12], 2: [15, 16], 3: [21, 22], 4: [23, 24] }
 
-gp.output(11, True)
-gp.output(12, True)
-gp.output(15, True)
-gp.output(16, True)
-gp.output(21, True)
-gp.output(22, True)
+#For loop to turn on the relevant pins
+for x in range(0,len(arducam_pins)):
+   pins = arducam_pins[x+1]
+   for pin in pins:
+        gp.setup(pin, gp.OUT)
+        gp.output(pin, True)
+
+#algorithm to extract serial number of the Raspberry Pi
+serialfile = open('/proc/cpuinfo','r')
+for line in serialfile:
+    if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+serialfile.close()
+
+#Using all pathways as global variables
+pathway = pathway1 = pathway2 = pathway3 = pathway4 = "global"
+
 
 def main():
-    f = open('/proc/cpuinfo','r')
-    for line in f:
-      if line[0:6]=='Serial':
-        cpuserial = line[10:26]
-    f.close()
-
-    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/" > out.txt' %cpuserial)
-#    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/Module2" > out2.txt' %cpuserial)
-#    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/Module3" > out3.txt' %cpuserial)
-#    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/Module4" > out4.txt' %cpuserial)
-    f = open('/home/pi/out.txt','r')
-    line = f.readlines()
-    char1 = len(line[1])
-    pathway1 = line[1][29:char1]
-    char2 = len(line[2])
-    pathway2 = line[2][29:char2]
-    char3 = len(line[3])
-    pathway3 = line[3][29:char3]
-    char4 = len(line[4])
-    pathway4 = line[4][29:char4]
-    f.close()
-#    f = open('/home/pi/out1.txt','r')
-#    char1 = len(line)
-#    pathway1 = line[29:char1]            
-#    f.close()
-#    
-#    g = open('/home/pi/out2.txt','r')
-#    char2 = len(line)
-#    pathway2 = line[29:char2]            
-#    g.close()
-#    
-#    h = open('/home/pi/out3.txt','r')
-#    char3 = len(line)
-#    pathway3 = line[29:char3]            
-#    h.close()
-#    
-#    i = open('/home/pi/out4.txt','r')
-#    char4 = len(line)
-#    pathway4 = line[29:char4]            
-#    i.close()
-    
-    print(pathway1)
-    print(pathway2)
-    print(pathway3)
-    print(pathway4)
-#
-#    pathway2 = ""
-#    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/Module2" > out2.txt' %cpuserial)
-#
-#    f = open('/home/pi/out2.txt','r')
-#
-#    text = f.read()
-#    characters=0
-#
-#    for i in text:
-#        characters += len(i)
-#
-#    f.close()
-#
-#    f = open('/home/pi/out2.txt','r')
-#    for line in f:
-#        pathway2 = line[29:characters]
-#            
-#    f.close()
-#
-#    pathway3 = ""
-#    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/Module3" > out3.txt' %cpuserial)
-#
-#    f = open('/home/pi/out3.txt','r')
-#
-#    text = f.read()
-#    characters=0
-#
-#    for i in text:
-#        characters += len(i)
-#
-#    f.close()
-#
-#    f = open('/home/pi/out3.txt','r')
-#    for line in f:
-#        pathway3 = line[29:characters]
-#            
-#    f.close()
-#
-#    pathway4 = ""
-#    os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s/Stack1/Module4" > out4.txt' %cpuserial)
-#
-#    f = open('/home/pi/out4.txt','r')
-#
-#    text = f.read()
-#    characters=0
-#
-#    for i in text:
-#        characters += len(i)
-#
-#    f.close()
-#
-#    f = open('/home/pi/out4.txt','r')
-#    for line in f:
-#        pathway4 = line[29:characters]
-#            
-#    f.close()
+    #using the pathway in the define structure by declaring it
+    if (os.path.exists('/home/pi/out.txt') == False):
+        #if there is no output file created, a new one is generated with recursive listings associated with the Pi's serial number
+        os.system('s3cmd ls -r s3://inhouseproduce-sites | grep "%s" > out.txt' %cpuserial)
+        #Opening and extrapolating data from the newly generated output files in one function called get_pathways()
+        get_pathways()
+    else:
+        #if out.txt is already present, the script will not send the command for the recursive listing and get the pathways from the already generated out.txt file
+        get_pathways()
 
     while True:
-        gp.output(7, False)
-        gp.output(11, False)
-        gp.output(12, True)
-        camera.rotation = 180
-        camera.start_preview()
-        time.sleep(3)
-        date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-        camera.capture("/home/pi/camera1_%s.jpg" %date)
-        camera.stop_preview()
-        os.system('s3cmd put camera1_%s.jpg %s' %(date, pathway1))
-        os.system('rm /home/pi/camera1_%s.jpg' %date)
-        time.sleep(30)
+         #First Camera Operation (Module1)
+         gp.output(7, False); gp.output(11, False); gp.output(12, True)
+         #main pathway set to pathway1
+         pathway = pathway1
+         #function that implements the whole camera capture
+         camera_process()
 
-        gp.output(7, True)
-        gp.output(11, False)
-        gp.output(12, True)
-        camera.rotation = 270
-        camera.start_preview()
-        time.sleep(3)
-        date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-        camera.capture("/home/pi/camera2_%s.jpg" %date)
-        camera.stop_preview() 
-        os.system('s3cmd put camera2_%s.jpg %s' %(date, pathway2))
-        os.system('rm /home/pi/camera2_%s.jpg' %date)
-        time.sleep(30)
+         #Second Camera Operation (Module2)
+         gp.output(7, True); gp.output(11, False); gp.output(12, True)
+         #main pathway set to pathway2
+         pathway = pathway2
+         #function that implements the whole camera capture
+         camera_process()
 
-        gp.output(7, False)
-        gp.output(11, True)
-        gp.output(12, False)
-        camera.rotation = 270
-        camera.start_preview()
-        time.sleep(3)
-        date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-        camera.capture("/home/pi/camera3_%s.jpg" %date)
-        camera.stop_preview()
-        pathway = ""
-        os.system('s3cmd put camera3_%s.jpg %s' %(date, pathway3))
-        os.system('rm /home/pi/camera3_%s.jpg' %date)
-        time.sleep(30)
+         #Third Camera Operation (Module3)
+         gp.output(7, False); gp.output(11, True); gp.output(12, False)
+         #main pathway set to pathway3
+         pathway = pathway3
+         #function that implements the whole camera capture
+         camera_process()
 
-        gp.output(7, True)
-        gp.output(11, True)
-        gp.output(12, False)
-        camera.rotation = 270
-        camera.start_preview()
-        time.sleep(3)
-        date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-        camera.capture("/home/pi/camera4_%s.jpg" %date)
-        camera.stop_preview()
-        os.system('s3cmd put camera4_%s.jpg %s' %(date, pathway4))
-        os.system('rm /home/pi/camera4_%s.jpg' %date)
-        time.sleep(1800)
+         #Fourth Camera Operation (Module4)
+         gp.output(7, True); gp.output(11, True); gp.output(12, False)
+         #main pathway set to pathway4
+         pathway = pathway4
+         #function that implements the whole camera capture
+         camera_process()
 
+         time.sleep(1800)
+
+def get_pathways()
+   #using the global pathway variable and assigning them appropriate paths
+   global pathway1, pathway2, pathway3, pathway4
+   f = open('/home/pi/out.txt','r')
+   #reading lines of the file one by one
+   lines = f.readlines()
+   for line in lines:
+       #following lines get the desired listing and strip the unnecessary string from the pathway
+       if (line.find('Module1') >= 0):
+           #29th character onwards gives the correct pathway
+           pathway1 = line[29:]
+       elif (line.find('Module2') >= 0):
+           #29th character onwards gives the correct pathway
+           pathway2 = line[29:]
+       elif (line.find('Module3') >= 0):
+           #29th character onwards gives the correct pathway
+           pathway3 = line[29:]
+       elif (line.find('Module4') >= 0):
+           #29th character onwards gives the correct pathway
+           pathway4 = line[29:]
+
+
+#operation of the user-defined function 'camera_process()'
+def camera_process():
+    camera.rotation = 270
+    camera.start_preview()
+    #at least 2 seconds of sleep time required for the camera to focus
+    time.sleep(3)
+    #Using OS system commands to first put the image in the desired folder and then deleting the image from the Pi
+    os.system('s3cmd put camera_%s.jpg %s' %(date, pathway))
+    os.system('rm /home/pi/camera_%s.jpg' %date)
+    time.sleep(60)
+
+#initializing the main function
 if __name__ == "__main__":
-    
-    main()
-
