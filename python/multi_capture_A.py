eb903ec2ac612e567fbea413943a1542b72b94c3
ocasta181 <ocasta181@gmail.com>
2019-08-07 12:08:37 -0700

    refactor: more functions, fix indendation, ordering, and spacing

--- python/multi_capture_A.py
+++ python/multi_capture_A.py
@@ -10,26 +10,22 @@ import time
 import datetime
 from picamera import PiCamera
 
-#using camera as the variable to control PiCamera()
-camera = PiCamera()
+#Using all pathways as global variables
+pathway = pathway1 = pathway2 = pathway3 = pathway4 = "global"
 
-#set warnings OFF
-gp.setwarnings(False)
-gp.setmode(gp.BOARD)
 
-#Pin 7 needed for all cameras
-gp.setup(7, gp.OUT)
+def setPins():
+    gp.setup(7, gp.OUT) #Pin 7 needed for all cameras
 
-#Listed pins according to arducam adapter number
-arducam_pins = {
-     1: [11, 12], 2: [15, 16], 3: [21, 22], 4: [23, 24] }
+    #Listed pins according to arducam adapter number
+    arducam_pins = {1: [11, 12], 2: [15, 16], 3: [21, 22], 4: [23, 24]}
 
-#For loop to turn on the relevant pins
-for x in range(0,len(arducam_pins)):
-   pins = arducam_pins[x+1]
-   for pin in pins:
-        gp.setup(pin, gp.OUT)
-        gp.output(pin, True)
+    #For loop to turn on the relevant pins
+    for x in range(len(arducam_pins)):
+        for pin in arducam_pins[x+1]:
+            gp.setup(pin, gp.OUT)
+            gp.output(pin, True)
+
 
 #algorithm to extract serial number of the Raspberry Pi
 def getSerial():
@@ -38,16 +34,49 @@ def getSerial():
         if line[0:6]=='Serial':
             cpu_serial = line[10:26]
             serialfile.close()
+
             return cpu_serial
+
     serialfile.close()
-    
+
     return False
 
-#Using all pathways as global variables
-pathway = pathway1 = pathway2 = pathway3 = pathway4 = "global"
+
+def get_pathways()
+   global pathway1, pathway2, pathway3, pathway4
+   f = open('/home/pi/out.txt','r')
+   lines = f.readlines()
+   for line in lines:
+       #following lines get the desired listing and strip the unnecessary string from the pathway
+       if (line.find('Module1') >= 0):
+           pathway1 = line[29:] #29th character onwards gives the correct pathway
+       elif (line.find('Module2') >= 0):
+           pathway2 = line[29:]
+       elif (line.find('Module3') >= 0):
+           pathway3 = line[29:]
+       elif (line.find('Module4') >= 0):
+           pathway4 = line[29:]
+
+
+#operation of the user-defined function 'camera_process()'
+def camera_process(pathway):
+    camera = PiCamera()
+    camera.rotation = 270
+    camera.start_preview()
+    time.sleep(3) # >2 seconds of sleep time required for the camera to focus
+    date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+    camera.capture("/home/pi/camera_%s.jpg" %date)
+    camera.stop_preview()
+    os.system('s3cmd put camera_%s.jpg %s' %(date, pathway)) #push image to s3
+    os.system('rm /home/pi/camera_%s.jpg' %date) #delete image locally
 
 
 def main():
+    #set warnings OFF
+    gp.setwarnings(False)
+    gp.setmode(gp.BOARD)
+
+    setPins()
     #using the pathway in the define structure by declaring it
     if (os.path.exists('/home/pi/out.txt') == False):
         cpu_serial = getSerial()
@@ -63,64 +92,30 @@ def main():
         get_pathways()
 
     while True:
-         #First Camera Operation (Module1)
-         gp.output(7, False); gp.output(11, False); gp.output(12, True)
-         #function that implements the whole camera capture
-         camera_process(pathway1)
+        #First Camera Operation (Module1)
+        gp.output(7, False); gp.output(11, False); gp.output(12, True)
+        #function that implements the whole camera capture
+        camera_process(pathway1)
+        time.sleep(60)
 
-         #Second Camera Operation (Module2)
-         gp.output(7, True); gp.output(11, False); gp.output(12, True)
-         #function that implements the whole camera capture
-         camera_process(pathway2)
+        #Second Camera Operation (Module2)
+        gp.output(7, True); gp.output(11, False); gp.output(12, True)
+        #function that implements the whole camera capture
+        camera_process(pathway2)
+        time.sleep(60)
+        
 
-         #Third Camera Operation (Module3)
-         gp.output(7, False); gp.output(11, True); gp.output(12, False)
-         #function that implements the whole camera capture
-         camera_process(pathway3)
+        #Third Camera Operation (Module3)
+        gp.output(7, False); gp.output(11, True); gp.output(12, False)
+        #function that implements the whole camera capture
+        camera_process(pathway3)
+        time.sleep(60)
 
-         #Fourth Camera Operation (Module4)
-         gp.output(7, True); gp.output(11, True); gp.output(12, False)
-         #function that implements the whole camera capture
-         camera_process(pathway4)
-
-         time.sleep(1800)
-
-def get_pathways()
-   #using the global pathway variable and assigning them appropriate paths
-   global pathway1, pathway2, pathway3, pathway4
-   f = open('/home/pi/out.txt','r')
-   #reading lines of the file one by one
-   lines = f.readlines()
-   for line in lines:
-       #following lines get the desired listing and strip the unnecessary string from the pathway
-       if (line.find('Module1') >= 0):
-           #29th character onwards gives the correct pathway
-           pathway1 = line[29:]
-       elif (line.find('Module2') >= 0):
-           #29th character onwards gives the correct pathway
-           pathway2 = line[29:]
-       elif (line.find('Module3') >= 0):
-           #29th character onwards gives the correct pathway
-           pathway3 = line[29:]
-       elif (line.find('Module4') >= 0):
-           #29th character onwards gives the correct pathway
-           pathway4 = line[29:]
+        #Fourth Camera Operation (Module4)
+        gp.output(7, True); gp.output(11, True); gp.output(12, False)
+        #function that implements the whole camera capture
+        camera_process(pathway4)
+        time.sleep(1800)
 
 
-#operation of the user-defined function 'camera_process()'
-def camera_process(pathway):
-    camera.rotation = 270
-    camera.start_preview()
-    #at least 2 seconds of sleep time required for the camera to focus
-    time.sleep(3)
-    #new date and time stamp everytime
-    date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-    camera.capture("/home/pi/camera_%s.jpg" %date)
-    camera.stop_preview()
-    #Using OS system commands to first put the image in the desired folder and then deleting the image from the Pi
-    os.system('s3cmd put camera_%s.jpg %s' %(date, pathway))
-    os.system('rm /home/pi/camera_%s.jpg' %date)
-    time.sleep(60)
-
-#initializing the main function
 if __name__ == "__main__":
